7addc01a3434576384a40ce8482f107303294e67
Bjnck <basile.jonckheere@gmail.com>
2024-04-12 00:11:14 +0200

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {InfiniteScrollModule} from "ngx-infinite-scroll";
 import {ShareButtonsModule} from "ngx-sharebuttons/buttons";
 import {ShareIconsModule} from "ngx-sharebuttons/icons";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
     imports: [
-        BrowserModule, InfiniteScrollModule, ShareButtonsModule, ShareIconsModule
+        BrowserModule, InfiniteScrollModule, ShareButtonsModule, ShareIconsModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})
     ],
   providers: [],
   bootstrap: [AppComponent]
